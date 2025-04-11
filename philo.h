fb3c62fa84a2f9d797705a8c319d655777187917
Nofy261 <noufy33@yahoo.com>
2025-04-11 17:06:56 +0200

    first push philo

---
+++ philo.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/11 16:37:12 by nolecler          #+#    #+#             */
+/*   Updated: 2025/04/11 16:58:29 by nolecler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+
+
+typedef struct s_info
+{
+
+	
+}		t_info;
+
+
+typedef struct s_philo
+{
+	pthread_mutex_t  fork_r;
+	pthread_mutex_t  fork_l;
+	
+	
+}		t_philo;
+
+
+
+
+
+
+
+
+#endif
